f3ab07ce13cc6d42f54c329f1d563a7755b2a384
Adam Grady <adam.grady@live.com>
2021-12-19 19:49:03 -0600

    allowing for multiple sheets

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
 # vite-plugin-sheets
 
-Relaces `%sheets.<title>%` with content.
+Relaces `%sheets.<sheet>.<slot>%` with content.
 
-Useful when using Google Sheets as CMS.
+Useful when using Google Sheets as CMS. Spreadsheet must be shared with the service account.
 
-### Required Options
+Check out the [Sheets API](https://developers.google.com/sheets/api/) to get started. You must have a Google Cloud Project with the "Sheets API" enabled.
+
+#### Required Options
 
 - `creds`: Path to [service account credentials](https://console.cloud.google.com/iam-admin/serviceaccounts)
+- `spreadsheetId`: Defines what Spreadsheet ID to point to
 
-### Required Environment Variables
+#### Other Options
 
-- `SPREADSHEET_ID`: Defines what Spreadsheet ID to point to
-
-### Optional Environment Variables
-
-- `SHEET`: Sheet name to pull data from. Defaults to `Sheet1`
+- `slotColumn`: Defaults to `0`. Which column to look use as slots.
+- `contentColumn`: Defaults to `1`. Same as `slotColumn` but for the content.
+- `ignore`: Defaults to `true`. Weather or not to treat row `1` as a header column.
 
 
 ### Sample Config
@@ -23,15 +24,31 @@ Useful when using Google Sheets as CMS.
 export default {
   plugins: [
     sheets({
-      creds: "creds.json"
+      creds: "creds.json",
+      spreadsheetId: process.env.SPREADSHEET_ID
     })
   ]
 }
 ```
 
-### Sample `.env`
+### Sample Sheet Setup
 
-```
-SPREADSHEET_ID=<your-spreadsheet-id>
-SHEET=Sheet1
-```
+#### "Home" Sheet
+
+|Slot|Content|
+|-|-|
+|title|Awesome Title|
+|content|Cool content|
+
+#### "About" Sheet
+
+|Slot|Content|
+|-|-|
+|title|About Us|
+|something|Other stuff|
+
+The header row gets ignored by default.
+- `title` in the "Home" sheet would be mapped to `%sheets.home.title%`
+- `content` in the "Home" sheet would be mapped to `%sheets.home.content%`
+- `title` in the "About" sheet would be mapped to `%sheets.about.title%`
+- `something` in the "About" sheet would be mapped to `%sheets.about.something%`
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,77 @@
 import type { Plugin } from "vite";
 import path from "path";
-import { google } from "googleapis";
-import { loadEnv } from "vite";
+import { google, sheets_v4 } from "googleapis";
 
 interface Options {
-	creds: string
+	creds: string;
+	spreadsheetId: string;
+	slotColumn?: number;
+	contentColumn?: number;
+	ignore?: boolean;
 }
 
 export default (options: Options): Plugin => {
-	const env = loadEnv("", process.cwd(), "");
-	const regex = new RegExp(`%sheets\.(.+?)%`, "g");
+	const name = "vite-plugin-sheets";
+	const regex = new RegExp('%sheets\.([a-zA-Z0-9]+)\.([a-zA-Z0-9]+)%', "g");
 	const creds = path.join(process.cwd(), options.creds);
 	const auth = new google.auth.GoogleAuth({
 		keyFile: creds,
 		scopes: "https://www.googleapis.com/auth/spreadsheets.readonly"
 	});
+	const slotColumn = options.slotColumn || 0;
+	const contentColumn = options.contentColumn || 1;
+	const ignore = options.ignore || true;
+	let slots: Map<string, Map<string, string>> | undefined = undefined;
 
+	async function getSheetNames(api: sheets_v4.Sheets) {
+		const { data } = await api.spreadsheets.get({
+			spreadsheetId: options.spreadsheetId
+		});
+		if (!data.sheets)
+			throw new Error(`${name}: No sheets from API`);
+		return data.sheets.map(s => {
+			if (!s.properties || !s.properties.title) {
+				throw new Error(`${name}: No properties for sheets`);
+			}
+			return s.properties.title as string;
+		});
+	}
+
+	async function getSheetMap(api: sheets_v4.Sheets) {
+		const { data } = await api.spreadsheets.values.batchGet({
+			spreadsheetId: options.spreadsheetId,
+			ranges: await getSheetNames(api)
+		});
+		if (!data.valueRanges)
+			throw new Error(`${name}: missing ValueRanges`);
+		return data.valueRanges.reduce((sheets, vr) => {
+			if (!vr.range || !vr.values)
+				throw new Error(`${name}: ValueRange ${vr.range} missing range or values`);
+			if (ignore)
+				vr.values.shift();
+			const values = new Map<string, string>(vr.values?.map(v => [v[slotColumn], v[contentColumn]]));
+			sheets.set(vr.range.split("!")[0].toLowerCase(), values);
+			return sheets;
+		}, new Map<string, Map<string, string>>());
+	}
 
 	return {
-		name: "vite-plugin-sheets",
+		name,
 		async transform(code, id) {
 			const client = await auth.getClient();
 			const sheets = google.sheets({ version: "v4", auth: client });
-			const { data } = await sheets.spreadsheets.values.get({
-				auth,
-				spreadsheetId: env.SPREADSHEET_ID,
-				range: env.SHEET ?? "Sheet1"
-			});
-			data.values?.shift();
-			const slots = new Map(data.values?.map(v => [v[0], v[1]]));
+			if (!slots) {
+				console.log("loading sheets data...");
+				slots = await getSheetMap(sheets);
+			}
+
 			const result = { code };
 			const matches = code.matchAll(regex);
 			for (const match of matches) {
-				if (slots.has(match[1])) {
-					result.code = result.code.replace(match[0], slots.get(match[1]));
-				} else {
-					result.code = result.code.replace(match[0], `Error: ${match[1]} missing slot from sheet`);
+				const sheet = match[1];
+				const slot = match[2];
+				if (slots.has(sheet) && slots.get(sheet)?.has(slot)) {
+					result.code = result.code.replace(match[0], slots.get(sheet)?.get(slot) || "Error");
 				}
 			}
 
